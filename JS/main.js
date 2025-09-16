89f8f6eba757b6fb1562b05d78e5cdc417b16b54
Ehab Ahmed Abdelhamid Ahmed <159829287+EhapAhmeda@users.noreply.github.com>
2025-09-16 05:29:11 +0300

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+var typed = new Typed(".text", {
+  strings: [
+    "Full Stack .NET Developer",
+    "ASP.NET Core Specialist",
+    "SQL Server Expert",
+    "API and Microservices Builder",
+    "Frontend Developer",
+    "Backend Developer"
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  backDelay: 1000,
+  loop: true
+});
