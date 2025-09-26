cb59faa723f185c35a76baf2aedeed0d51479901
Ehab Ahmed Abdelhamid Ahmed <159829287+EhapAhmeda@users.noreply.github.com>
2025-09-26 11:54:25 +0300

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -12,3 +12,32 @@ var typed = new Typed(".text", {
   backDelay: 1000,
   loop: true
 });
+
+
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+const navLinks = document.querySelectorAll(".nav-link");
+
+
+menuOpenButton.addEventListener("click", () => {
+  document.body.classList.add("show-mobile-menu");
+});
+
+
+menuCloseButton.addEventListener("click", () => {
+  document.body.classList.remove("show-mobile-menu");
+});
+
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("show-mobile-menu");
+  });
+});
+
+
+
+
+
+
+
